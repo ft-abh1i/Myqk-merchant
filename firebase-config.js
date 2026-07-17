a760b3ba000361521dc58582abd09c02b22cf7ed
Abhijeet shrivastav <abhijeetshrivastava132@gmail.com>
2026-07-17 09:17:44 +0530

    Add shared Firebase configuration

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyDbNDNI1a69VDZmLo7Se6LNGPLD6A8_MmE',
+  authDomain: 'buyqk-rider.firebaseapp.com',
+  projectId: 'buyqk-rider',
+  storageBucket: 'buyqk-rider.firebasestorage.app',
+  messagingSenderId: '61147606971',
+  appId: '1:61147606971:web:d69dd4fcf5c0a0fea01e9e'
+};
