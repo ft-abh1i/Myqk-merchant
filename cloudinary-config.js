eceaf5dd04972d637f12f93aaad285654ad6255d
Abhijeet shrivastav <abhijeetshrivastava132@gmail.com>
2026-07-18 11:14:27 +0530

    Add Cloudinary public configuration

---
+++ cloudinary-config.js
@@ -0,0 +1,6 @@
+// Public browser configuration only.
+// Never add your Cloudinary API secret to this repository.
+export const cloudinaryConfig = Object.freeze({
+  cloudName: 'YOUR_CLOUD_NAME',
+  uploadPreset: 'YOUR_UNSIGNED_UPLOAD_PRESET'
+});
