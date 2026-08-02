023c3a72a8ef3bce0ff50e12a84cde7875b7a28f
Abhijeet shrivastav <abhijeetshrivastava132@gmail.com>
2026-08-02 14:55:24 +0530

    Refresh merchant activation Firebase module

--- auto-approve.js
+++ auto-approve.js
@@ -1,4 +1,4 @@
-import { firebaseConfig } from './firebase-config.js';
+import { firebaseConfig } from './firebase-config.js?v=20260802-buyqk-app-1';
 import { getApps, initializeApp } from 'https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js';
 import { getAuth, onAuthStateChanged } from 'https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js';
 import { doc, getDoc, getFirestore, serverTimestamp, updateDoc } from 'https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js';
